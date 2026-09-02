476a33aac17f2514e6b892a9b73c3bd9cbea8c99
Swedsid <58020625+Sasank-cyber@users.noreply.github.com>
2026-09-02 11:26:58 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool found = false;
+
+        for(int i=0;i<matrix.size();i++){
+            
+            found = binary_search(matrix[i].begin(),matrix[i].end(),target);
+            if(found){
+                return true;
+            }
+
+        }
+        return false;
+        
+        
+    }
+};
